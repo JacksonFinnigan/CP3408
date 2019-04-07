4afa25c5436cb3e4decafe0c604c32f09a19f60d
Froda94 <Froda94@users.noreply.github.com>
2019-04-07 23:35:59 +1000

    Animation Added
    - Animation added
    - Tidied up code a little
    - Added TODO list in script
    - fixed up some animation problems (there's still more)

--- Assignment/Assets/Scripts/PlayerController.cs
+++ Assignment/Assets/Scripts/PlayerController.cs
@@ -4,29 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    /// <summary>
+    /// PROBLEMS!
+    /// 
+    /// - stuck in walking and jumping anim when touching walls
+    /// - Add acceleration
+    /// - bouncing idle anim not showing
+    /// - Only half of jump animation is showing
+    /// 
+    /// 
+    /// </summary>
 
-    bool isGrounded = true;
-    bool isWalled = false;
-
-    Rigidbody2D rb;
 
     public float maxSpeed = 10;
     public float jumpSpeed = 8;
-    public float jumpDuration;
 
     public bool doubleJump = true;
-    public bool wallHitDoubleJumpOverride = true;
 
-    
-
-
-    // internal checks
+    public Animator animator;
 
 
-    float jmpDuration;
-    bool jumpKeyDown = false;
-    bool canVariableJump = false;
+    bool isGrounded = true;
+    bool isWalled = false;
+    bool facingRight = true;
+    bool moveVertical = false;
+    float moveHorizontal = 0f;
 
+
+    Rigidbody2D rb;
 
     // Use this for initialization
     private void Start()
@@ -34,53 +39,78 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetKeyDown("space");
+
+    }
+
     // FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        bool moveVertical = Input.GetKeyDown("space");
+        
 
-        //TODO Add acceleration here?
         // if grounded, can automatically double jump, if collision with wall, can't double jump
-        rb.velocity = new Vector2 (maxSpeed * moveHorizontal, rb.velocity.y);
+        if (moveHorizontal != 0)
+        {
+            rb.velocity = new Vector2(maxSpeed * moveHorizontal, rb.velocity.y);
+            animator.SetFloat("Speed", Mathf.Abs(moveHorizontal));
+        }
+
+        // flips char sprite when changing direction
+        if ((moveHorizontal > 0 && !facingRight) || (moveHorizontal < 0 && facingRight)){ Flip(); }
+
 
         if (moveVertical)
         {
+            animator.SetBool("Grounded", false);
+
+            // Set DJ to true if touching the ground
             if (isGrounded == true)
             {
-                isGrounded = false;
-                rb.velocity = new Vector2(maxSpeed * moveHorizontal, 0);
-                rb.velocity = new Vector2(maxSpeed * moveHorizontal, jumpSpeed);
-                doubleJump = true;
-
+                Jumping(isGrounded);
+                doubleJump = true;               
             }
+            // if doesn't have DJ the only SJ is enabled
             else
             {
                 if (doubleJump == true)
                 {
-                    rb.velocity = new Vector2(maxSpeed * moveHorizontal, 0);
-                    rb.velocity = new Vector2(maxSpeed * moveHorizontal, jumpSpeed);
-                    doubleJump = false;
+                    Jumping(doubleJump);
                 }
 
                 if (isWalled == true)
                 {
-                    rb.velocity = new Vector2(maxSpeed * moveHorizontal, jumpSpeed);
-                    isWalled = false;
+                    Jumping(isWalled);
+                    animator.SetBool("Walled", false);
                 }
 
             }
 
-            
+            moveVertical = false;
         }
 
     }
- 
-    void Update()
-    {
 
-        
+
+    void Jumping(bool changed)
+    {
+        rb.velocity = new Vector2(maxSpeed * moveHorizontal, 0);
+        rb.velocity = new Vector2(maxSpeed * moveHorizontal, jumpSpeed);
+        changed = false;
     }
+
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(Vector3.up * 180);
+    }
+
+
+
 
     //checking for collision with floor or wall
     void OnCollisionEnter2D(Collision2D theCollision)
@@ -89,10 +119,12 @@ public class PlayerController : MonoBehaviour
         if (theCollision.gameObject.name == "Floor")
         {
             isGrounded = true;
+            animator.SetBool("Grounded", isGrounded);
         }
         if (theCollision.gameObject.name == "Wall")
         {
             isWalled = true;
+            animator.SetBool("Grounded", isWalled);
         }
     }
 
