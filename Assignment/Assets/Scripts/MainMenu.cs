c0fe5fe26816cfa81da2bbb00b3b2763555f8a18
Froda94 <Froda94@users.noreply.github.com>
2019-05-19 00:42:15 +1000

    Added Pause and main menu
    Both work, except for graphics in the main menu
    Both also need to look nicer but it is currently good for as a placeholder

--- Assignment/Assets/Scripts/MainMenu.cs
+++ Assignment/Assets/Scripts/MainMenu.cs
@@ -1,14 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AudioMixer audioMixer;
+
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+    // Gets a list of resolutions, formats them and makes the currently used one selected
     void Start()
     {
-        
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        //create formatted string in order to add to options
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
     // Update is called once per frame
@@ -26,5 +51,27 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Quitted");
         Application.Quit();
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex, true);
+        Debug.Log(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
