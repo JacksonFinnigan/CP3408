02dcc4e15c186656f4b9063545ad0e17bf86c372
Froda94 <Froda94@users.noreply.github.com>
2019-05-26 16:02:01 +1000

    Save/Load menu added

---
+++ Assignment/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.IO;
+
+public class GameHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Save();
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Load();
+        }
+    }
+
+    public static int levelIndex;
+    int totalCoins;
+
+    private void Save()
+    {
+        totalCoins = PlayerController.totalCoinCount;
+        levelIndex = SceneManager.GetActiveScene().buildIndex;
+
+        Debug.Log("Saved: Index is: " + levelIndex + ", Coins is: " + totalCoins);
+
+        string[] lines = { levelIndex.ToString(), totalCoins.ToString() };
+        File.WriteAllLines("Assets/SaveData.txt", lines);
+
+    }
+
+    private void Load()
+    {
+        string[] lines = File.ReadAllLines("Assets/SaveData.txt");
+        Debug.Log(lines[0] + ":  " + lines[1]);
+        levelIndex = int.Parse(lines[0]);
+        totalCoins = int.Parse(lines[1]);
+
+        PlayerController.totalCoinCount = totalCoins;
+        SceneManager.LoadScene(levelIndex);
+    }
+}
